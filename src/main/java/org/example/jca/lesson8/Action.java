fb1d33a4ca8eb2921093737e459aa4d0682b07b5
Keilis <85296911+Keilis@users.noreply.github.com>
2021-09-23 13:52:41 +0300

    Revert "Homework 8 done"

--- src/main/java/org/example/jca/lesson8/Action.java
+++
@@ -1,9 +0,0 @@
-package org.example.jca.lesson8;
-
-public interface Action {
-    boolean run(int line);
-
-    boolean jump(int line);
-
-    String getName();
-}
--- src/main/java/org/example/jca/lesson8/Barrier.java
+++
@@ -1,5 +0,0 @@
-package org.example.jca.lesson8;
-
-public interface Barrier {
-    boolean tries(Action action);
-}
